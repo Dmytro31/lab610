38d9f40aa2702ab3ea53e548b190e8a4b3fe2553
Dmytro31 <99250554+Dmytro31@users.noreply.github.com>
2022-05-18 13:21:37 +0300

    Update Management.java

--- src/com/company/Management.java
+++ src/com/company/Management.java
@@ -22,27 +22,51 @@ class Management extends ComputerScience {
     }
     public void semester(int s){
         if(s==1){
-            System.out.println("The first semester");
+            System.out.println("first course");
             System.out.println("             Предмети         ");
-            System.out.println("Інженерна та комп'ютеран графіка");
-            System.out.println("Українська мова і культура");
-            System.out.println("Правознавство та правові основи цивільного захисту України");
-            System.out.println("Лінійна алгебра та аналітична геометрія");
-            System.out.println("Математичний аналіз");
-            System.out.println("Фізика");
-            System.out.println("Основи програмування");
-            System.out.println("Дискретна математика");
-        }else if(s==2){
-            System.out.println("Second semester");
-            System.out.println("           Предмети          ");
-            System.out.println("Історія української державності");
+            System.out.println("Економічна теорія (мікроекономіка та макроекономіка)");
+            System.out.println("Лідерство та стилі керівництва");
+            System.out.println("Правознавство");
+            System.out.println("Вступ до фаху (Менеджмент)");
+            System.out.println("Теорія організацій");
+            System.out.println("Господарське та трудове право");
+            System.out.println("Українська мова та культура");
             System.out.println("Іноземна мова");
-            System.out.println("Чисельні методи");
-            System.out.println("Основи WEB - розробки");
-            System.out.println("Математичний аналіз");
+            System.out.println("Вища математика");
+            System.out.println("Інформатика та комп'ютерна техніка");
+            System.out.println("Основи психології");
+            System.out.println("Основи підприємництва");
+            System.out.println("Основи економічної науки");
+            System.out.println("Фізичне виховання");
+            System.out.println("Історія української державності");
+            System.out.println("Політологія");
+            System.out.println("Мікроекономіка");
+            System.out.println("Психологія управління");
+        }else if(s==2){
+            System.out.println("second course");
+            System.out.println("           Предмети          ");
+            System.out.println("Маркетинг");
+            System.out.println("Трудове право");
+            System.out.println("Інформаційні системи і технології в менеджменті");
+            System.out.println("Організація кадрової роботи");
+            System.out.println("Автомобіль та безпека дорожнього руху");
+            System.out.println("Бізнес аналіз");
+            System.out.println("Основи підприємництва");
+            System.out.println("Теорія та практика бізнес-планування");
+            System.out.println("Контролінг");
+            System.out.println("Основи діловодства");
             System.out.println("Фізика");
             System.out.println("Основи програмування");
-            System.out.println("Дискретна математика");
+            System.out.println("Теорія та практика бізнес-планування");
+            System.out.println("Економіка цивільної безпеки");
+            System.out.println("Основи теорії систем та системного аналізу");
+            System.out.println("Єдина державна система цивільного захисту");
+            System.out.println("Планування діяльності та управління розвитком організації");
+            System.out.println("Фінанси, гроші та кредит");
+            System.out.println("Філософія");
+            System.out.println("Теорія організацій");
+            System.out.println("Основи трудового права та організація кадрової роботи");
+            System.out.println("Організація економічної роботи на підприємстві");
         }else{
             System.out.println("Invalid value entered");
         }
